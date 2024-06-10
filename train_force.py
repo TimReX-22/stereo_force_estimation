07fa1e10ccd98ee47a49a7282ed3a76fa55e8dba
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-06-09 18:29:45 -0700

    improved force prediction network

--- train_force.py
+++ train_force.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from image_reconstruction import DisparityEstimationConfig
-from force_estimation import InteractionForcePredictionNetwork
+from force_estimation_net import ForcePredictionNetwork
 from dataset import StereoForceDataset
 from loss import total_loss
 from tqdm import tqdm
@@ -21,11 +21,16 @@ def force_loss(predicted_force, true_force):
     return nn.MSELoss()(predicted_force, true_force)
 
 
+def rmse(predicted_force, true_force):
+    return torch.sqrt(torch.mean((predicted_force - true_force) ** 2))
+
+
 def combined_loss(left_image, right_image, reconstructed_right, reconstructed_left, disparity, predicted_force, true_force):
     reconstruction_loss = total_loss(
         left_image, right_image, reconstructed_right, reconstructed_left, disparity)
     force_pred_loss = force_loss(predicted_force, true_force)
-    return reconstruction_loss + force_pred_loss
+    combined = reconstruction_loss + force_pred_loss
+    return combined, reconstruction_loss, force_pred_loss
 
 
 def main():
@@ -34,7 +39,7 @@ def main():
     num_layers = 18
     pretrained = True
     batch_size = 8
-    num_epochs = 20
+    num_epochs = 10
     learning_rate = 1e-5
     num_disparities = 64
     train_test_split_ratio = 0.8
@@ -58,7 +63,7 @@ def main():
         f"[INFO] Loaded Trainset with {train_size} samples and Testset with {test_size} samples!")
     disparity_config = DisparityEstimationConfig(
         num_layers, pretrained, num_disparities)
-    model = InteractionForcePredictionNetwork(disparity_config)
+    model = ForcePredictionNetwork(disparity_config)
     model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
@@ -67,6 +72,8 @@ def main():
     test_losses = []
     train_mse = []
     test_mse = []
+    train_rmse = []
+    test_rmse = []
     train_disparity_loss = []
     test_disparity_loss = []
 
@@ -79,6 +86,7 @@ def main():
                 model.eval()
             running_loss = 0.0
             running_mse = 0.0
+            running_rmse = 0.0
             running_disparity_loss = 0.0
             for left_image, right_image, true_force in tqdm(dataloaders[phase]):
                 left_image, right_image, true_force = left_image.to(
@@ -89,24 +97,29 @@ def main():
                         left_image, right_image)
                     loss, disparity_loss, mse_loss = combined_loss(
                         left_image, right_image, reconstructed_right, reconstructed_left, disparity, predicted_force, true_force)
+                    rmse_loss = rmse(predicted_force, true_force)
                     if phase == "train":
                         loss.backward()
                         optimizer.step()
                 batch_size = left_image.size(0)
                 running_loss += loss.item() * batch_size
                 running_mse += mse_loss.item() * batch_size
+                running_rmse += rmse_loss.item() * batch_size
                 running_disparity_loss += disparity_loss.item() * batch_size
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_mse = running_mse / len(dataloaders[phase].dataset)
+            epoch_rmse = running_rmse / len(dataloaders[phase].dataset)
             epoch_disparity_loss = running_disparity_loss / \
                 len(dataloaders[phase].dataset)
             if phase == "train":
                 train_losses.append(epoch_loss)
                 train_mse.append(epoch_mse)
+                train_rmse.append(epoch_rmse)
                 train_disparity_loss.append(epoch_disparity_loss)
             else:
                 test_losses.append(epoch_loss)
                 test_mse.append(epoch_mse)
+                test_rmse.append(epoch_rmse)
                 test_disparity_loss.append(epoch_disparity_loss)
         print(f"Train Loss: {train_losses[-1]:.4f} \t Test Loss: {test_losses[-1]:.4f} \t Train MSE: {train_mse[-1]:.4f} \t Test MSE: {test_mse[-1]:.4f} \t Train Disparity Loss: {train_disparity_loss[-1]:.4f} \t Test Disparity Loss: {test_disparity_loss[-1]:.4f}")
         scheduler.step()
@@ -115,6 +128,8 @@ def main():
                               test_losses, "Combined Loss", "Loss")
     util.save_and_plot_losses(num_epochs, train_mse,
                               test_mse, "Force MSE", "MSE")
+    util.save_and_plot_losses(num_epochs, train_rmse,
+                              test_rmse, "Force RMSE", "RMSE")
     util.save_and_plot_losses(num_epochs, train_disparity_loss,
                               test_disparity_loss, "Disparity Loss", "Loss")
     torch.save(model.state_dict(), "weights/force_estimation_weights.pth")
